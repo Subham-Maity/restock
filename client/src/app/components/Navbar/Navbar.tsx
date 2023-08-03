bbb5d73eedde5d92e95c66b501a1c1c60dc23bab
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-08-03 22:59:28 +0530

    Theme setup (80%)

---
+++ client/src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <div className="bg-yellow-600 dark:bg-gray-700 ">
+            Navbar
+        </div>
+    );
+};
+
+export default Navbar;
