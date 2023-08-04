a0299baa107471759dc1431f2d33573a1f519ab1
Suman Mandal <newsumanm@gmail.com>
2023-08-04 23:55:24 +0530

    Url added

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["tailwindui.com", "raw.githubusercontent.com"],
+    domains: ["tailwindui.com","images.unsplash.com", "raw.githubusercontent.com"],
   },
 };
 
