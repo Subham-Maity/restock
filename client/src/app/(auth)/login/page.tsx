ba0e4d2de8c45a5a30a50e36bd37cf93a269367a
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-08-05 22:32:03 +0530

    New Changes

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "@/app/components/auth/components/Login";
+
+const Page = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default Page;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Image from "next/image";
 import "./globals.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import Switcher from "@/app/components/Mode/Switcher";
 import { HomeProductFilter } from "@/app/components/products/pages/Home/ProductList";
+import Banner from "@/app/components/Navbar/Banner/Home/Banner";
 
 export default function Home() {
   return (
     <main>
+      <Banner />
       <HomeProductFilter />
     </main>
   );
