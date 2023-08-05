cf255a82c5af5bb17d68d448e9dfc04243f30890
Suman Mandal <newsumanm@gmail.com>
2023-08-05 15:36:01 +0530

    Updated Mobile dark theam

--- client/src/app/components/products/pages/Home/ProductList.tsx
+++ client/src/app/components/products/pages/Home/ProductList.tsx
@@ -304,14 +304,14 @@ export const HomeProductFilter = () => {
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white dark:bg-[#2f3349] py-4 pb-12 shadow-xl">
                   <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">
+                    <h2 className="text-lg font-medium text-gray-900 dark:text-white">
                       Filters
                     </h2>
                     <button
                       type="button"
-                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400 dark:bg-[#1c1f2d]"
                       onClick={() => setMobileFiltersOpen(false)}
                     >
                       <span className="sr-only">Close menu</span>
@@ -339,13 +339,14 @@ export const HomeProductFilter = () => {
                       <Disclosure
                         as="div"
                         key={section.id}
-                        className="border-t border-gray-200 px-4 py-6 dark:dg-[#2f3349] dark:text-white"
+                        className="border-t border-gray-200 px-4 py-6 dark:dg-[#2f3349] dark:text-white "
                       >
                         {({ open }) => (
                           <>
-                            <h3 className="-mx-2 -my-3 flow-root">
-                              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                <span className="font-medium text-red-900">
+                            <h3 className="-mx-2 -my-3 flow-root ">
+                              <Disclosure.Button className="flex w-full items-center justify-between px-2 py-3 hover:text-gray-500 dark:hover:bg-[#34384e] dark:hover:text-[#8669de]">
+                                {/*Mobile text*/}
+                                <span className="font-medium ">
                                   {section.name}
                                 </span>
                                 <span className="ml-6 flex items-center">
@@ -380,7 +381,7 @@ export const HomeProductFilter = () => {
                                     />
                                     <label
                                       htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                      className="ml-3 min-w-0 flex-1 text-gray-500"
+                                      className="ml-3 min-w-0 flex-1 text-gray-500 dark:text-white dark:hover:text-[#8669de]"
                                     >
                                       {option.label}
                                     </label>
