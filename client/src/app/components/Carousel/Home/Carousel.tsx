e8867793ab5d4cc80012808672122bbcad27e593
Subhasish Negel <negelsubho@gmail.com>
2023-08-16 03:24:46 +0530

    carousel, banner fixes

--- client/src/app/components/Carousel/Home/Carousel.tsx
+++ client/src/app/components/Carousel/Home/Carousel.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Slider from "react-slick";
@@ -22,69 +23,49 @@ const settings = {
   initialSlide: 0,
   cssEase: "linear",
 };
-// Define a constant array of image data
-const images: Image[] = [
-  {
-    id: 1,
-    src: "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "First image",
-  },
-  {
-    id: 2,
-    src: "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "Second image",
-  },
-  {
-    id: 3,
-    src: "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "Third image",
-  },
-];
 
-const Carousel = () => {
+
+const Carousel: React.FC<{ props: Image[] }> = ({ props }) => {
   const router = useRouter();
+
   return (
-    <div className="object-cover mx-auto rounded-2xl">
-      <div className="lg:mx-40">
-        <div className="flex items-center justify-center">
-          <div className="justify-center">
-            <div>
-              <div className="grid grid-cols-1 gap-4 max-w-[800px] h-auto rounded-lg overflow-hidden mt-4">
-                <div style={{ overflow: "hidden", padding: "2px 30px 45px" }}>
-                  <Slider {...settings}>
-                    {images.map((image) => {
-                      return (
-                        <>
+      <div className="object-cover mx-auto rounded-2xl">
+        <div className="lg:mx-40">
+          <div className="flex items-center justify-center">
+            <div className="justify-center">
+              <div>
+                <div className="grid grid-cols-1 gap-4 max-w-full h-auto rounded-lg overflow-hidden mt-4">
+                  <div style={{ overflow: "hidden", padding: "2px 30px 45px" }}>
+                    <Slider {...settings}>
+                      {props.map((image) => (
                           <div
-                            key={image.id}
-                            className="flex items-center justify-center relative"
+                              key={image.id}
+                              className="flex items-center justify-center relative"
                           >
                             <Image
-                              className={`object-cover lg:min-h-full lg:min-w-full hover:scale-[1.05] hover:rounded-2xl rounded-2xl transiton-all ease-in-out duration-500`}
-                              onClick={() => {
-                                if (image.href != null) {
-                                  router.push(image.href);
-                                }
-                              }}
-                              src={image.src}
-                              alt={image.alt}
-                              width={500}
-                              height={500}
-                              layout="intrinsic"
-                              objectFit="cover"
+                                className={`object-cover lg:min-h-full lg:min-w-full hover:scale-[1.05] hover:rounded-2xl rounded-2xl transiton-all ease-in-out duration-500`}
+                                onClick={() => {
+                                  if (image.href != null) {
+                                    router.push(image.href);
+                                  }
+                                }}
+                                src={image.src}
+                                alt={image.alt}
+                                width={500}
+                                height={500}
+                                layout="intrinsic"
+                                objectFit="cover"
                             />
                           </div>
-                        </>
-                      );
-                    })}
-                  </Slider>
+                      ))}
+                    </Slider>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
