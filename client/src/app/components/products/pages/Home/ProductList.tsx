b40d1fb6cf1fa6e4840123df77cb957987359098
Subhasish Negel <negelsubho@gmail.com>
2023-08-15 22:49:13 +0530

    carousel, margin fixes
    created advance carousel, done some home page margin fixes

--- client/src/app/components/products/pages/Home/ProductList.tsx
+++ client/src/app/components/products/pages/Home/ProductList.tsx
@@ -407,7 +407,7 @@ export const HomeProductFilter = () => {
         </Transition.Root>
 
         <main className=" mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
-          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
               New Arrivals
             </h1>
