4a78a9d582e688eb29209d6e1d3e11886f814737
Subhasish Negel <negelsubho@gmail.com>
2023-08-23 23:21:10 +0530

    added margin on product list top

--- client/src/app/components/products/pages/pc-components/ProductList.tsx
+++ client/src/app/components/products/pages/pc-components/ProductList.tsx
@@ -257,7 +257,7 @@ export const PcComponentFilter = () => {
                 leaveTo="translate-x-full"
               >
                 <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white dark:bg-[#2f3349] py-4 pb-12 shadow-xl">
-                  <div className="flex items-center justify-between px-4">
+                  <div className="mt-[4.5rem] flex items-center justify-between px-4">
                     <h2 className="text-lg font-medium text-gray-900 dark:text-white">
                       Filters
                     </h2>
