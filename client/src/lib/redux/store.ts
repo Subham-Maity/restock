68473a13f104b429e3de8151822b49d69ec7a924
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-08-26 03:09:09 +0530

    Pagination & Proper Typescript

--- client/src/lib/redux/store.ts
+++ client/src/lib/redux/store.ts
@@ -1,6 +1,8 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../../app/components/products/pages/pc-components/productListSlice";
 
 export const store = configureStore({
-    reducer: {product: productReducer},
+  reducer: { product: productReducer },
 });
+
+export type AppDispatch = typeof store.dispatch;
