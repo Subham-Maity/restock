bdcb8f344de7f1e34d85619d507d999713376ef3
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-09-04 04:44:52 +0530

    Embla TypeScript Fix + Image Replace

--- client/src/app/components/Carousel/Carousel2/EmblaCarousel.tsx
+++ client/src/app/components/Carousel/Carousel2/EmblaCarousel.tsx
@@ -1,61 +1,61 @@
-'use client';
-import React, { useCallback, useEffect, useState } from 'react'
-import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react'
-import imageByIndex from './imageByIndex'
-import Autoplay from 'embla-carousel-autoplay'
+"use client";
+import React, {useCallback, useEffect, useState} from "react";
+import useEmblaCarousel, {EmblaOptionsType} from "embla-carousel-react";
+import imageByIndex from "./imageByIndex";
+import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
 import {flushSync} from "react-dom";
 
-const TWEEN_FACTOR = 1.2
+const TWEEN_FACTOR = 1.2;
 
 type PropType = {
-    slides: number[]
-    options?: EmblaOptionsType
-}
+    slides: number[];
+    options?: EmblaOptionsType;
+};
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-    const { slides, options } = props
+    const {slides, options} = props;
     const autoplayOptions = {
         delay: 3000,
-        stopOnInteraction:false
-    }
-    const plugins = [Autoplay(autoplayOptions)]
-    // @ts-ignore
-    const [emblaRef, emblaApi] = useEmblaCarousel(options, plugins)
-    const [tweenValues, setTweenValues] = useState<number[]>([])
+        stopOnInteraction: false,
+    };
+    const plugins = [Autoplay(autoplayOptions)];
+
+    const [emblaRef, emblaApi] = useEmblaCarousel(options, plugins);
+    const [tweenValues, setTweenValues] = useState<number[]>([]);
 
     const onScroll = useCallback(() => {
-        if (!emblaApi) return
+        if (!emblaApi) return;
 
-        const engine = emblaApi.internalEngine()
-        const scrollProgress = emblaApi.scrollProgress()
+        const engine = emblaApi.internalEngine();
+        const scrollProgress = emblaApi.scrollProgress();
 
         const styles = emblaApi.scrollSnapList().map((scrollSnap, index) => {
-            let diffToTarget = scrollSnap - scrollProgress
+            let diffToTarget = scrollSnap - scrollProgress;
 
             if (engine.options.loop) {
                 engine.slideLooper.loopPoints.forEach((loopItem) => {
-                    const target = loopItem.target()
+                    const target = loopItem.target();
                     if (index === loopItem.index && target !== 0) {
-                        const sign = Math.sign(target)
-                        if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress)
-                        if (sign === 1) diffToTarget = scrollSnap + (1 - scrollProgress)
+                        const sign = Math.sign(target);
+                        if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress);
+                        if (sign === 1) diffToTarget = scrollSnap + (1 - scrollProgress);
                     }
-                })
+                });
             }
-            return diffToTarget * (-1 / TWEEN_FACTOR) * 100
-        })
-        setTweenValues(styles)
-    }, [emblaApi, setTweenValues])
+            return diffToTarget * (-1 / TWEEN_FACTOR) * 100;
+        });
+        setTweenValues(styles);
+    }, [emblaApi, setTweenValues]);
 
     useEffect(() => {
-        if (!emblaApi) return
-        onScroll()
-        emblaApi.on('scroll', () => {
-            flushSync(() => onScroll())
-        })
-        emblaApi.on('reInit', onScroll)
-    }, [emblaApi, onScroll])
+        if (!emblaApi) return;
+        onScroll();
+        emblaApi.on("scroll", () => {
+            flushSync(() => onScroll());
+        });
+        emblaApi.on("reInit", onScroll);
+    }, [emblaApi, onScroll]);
 
     return (
         <div className="embla">
@@ -71,14 +71,16 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
                                     className="embla__parallax__layer"
                                     style={{
                                         ...(tweenValues.length && {
-                                            transform: `translateX(${tweenValues[index]}%)`
-                                        })
+                                            transform: `translateX(${tweenValues[index]}%)`,
+                                        }),
                                     }}
                                 >
                                     <Image
-                                        className="embla__slide__img embla__parallax__img"
+                                        className="embla__slide__img embla__parallax__img object-fill h-24"
                                         src={imageByIndex(index)}
                                         alt="Your alt text"
+                                        width={2000}
+                                        height={2000}
                                     />
                                 </div>
                             </div>
@@ -87,7 +89,7 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default EmblaCarousel
+export default EmblaCarousel;
--- client/src/app/components/Carousel/Carousel2/imageByIndex.ts
+++ client/src/app/components/Carousel/Carousel2/imageByIndex.ts
@@ -1,12 +1,20 @@
-import image1 from '../images/banner1.jpg'
-import image2 from '../images/banner2.jpg'
-import image3 from '../images/banner3.jpg'
-import image4 from '../images/banner2.jpg'
-// import image4 from '../images/slide-4.jpg'
+import image1 from "../../../../../public/BannerPoster/1.jpg";
+import image2 from "../../../../../public/BannerPoster/2.jpg";
+import image3 from "../../../../../public/BannerPoster/3.jpg";
+import image4 from "../../../../../public/BannerPoster/4.jpg";
+import image5 from "../../../../../public/BannerPoster/5.jpg";
+import image6 from "../../../../../public/BannerPoster/6.jpg";
+import { StaticImageData } from "next/image";
 
-// @ts-ignore
-export const images: string[] = [image1, image2, image3, image4]
+export const images: StaticImageData[] = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+];
 
-const imageByIndex = (index: number): string => images[index % images.length]
+const imageByIndex = (index: number) => images[index % images.length];
 
-export default imageByIndex
+export default imageByIndex;
