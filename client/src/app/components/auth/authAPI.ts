4af112a8ce7fbeb169282c32676ed263cdfa55b5
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-09-19 18:38:51 +0530

    Address Bug : API

--- client/src/app/components/auth/authAPI.ts
+++ client/src/app/components/auth/authAPI.ts
@@ -47,17 +47,24 @@ export async function checkUser(loginInfo: LoginInfo): Promise<UserResponse> {
   });
 }
 
-export function updateUser(update: User) {
-  return new Promise(async (resolve) => {
+export async function updateUser(update: User): Promise<{ data: User }> {
+  try {
     const response = await fetch(
-      "https://restock-api.onrender.com/users/" + update.id,
-      {
-        method: "PATCH",
-        body: JSON.stringify(update),
-        headers: { "content-type": "application/json" },
-      },
+        "https://restock-api.onrender.com/users/" + update.id,
+        {
+          method: "PATCH",
+          body: JSON.stringify(update),
+          headers: { "content-type": "application/json" },
+        },
     );
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+
     const data = await response.json();
-    resolve({ data });
-  });
+    return { data };
+  } catch (error: any) {
+    throw new Error(`Error updating user: ${error.message}`);
+  }
 }
