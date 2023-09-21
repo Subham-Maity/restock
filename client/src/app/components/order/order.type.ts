d6f2f97473448bcb83104d8e6e8598f4864d0b65
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-09-21 20:37:31 +0530

    Order API & Slice

---
+++ client/src/app/components/order/order.type.ts
@@ -0,0 +1,50 @@
+
+
+interface Address {
+    name: string;
+    email: string;
+    phone: string;
+    street: string;
+    city: string;
+    state: string;
+    pinCode: string;
+}
+
+interface User {
+    email: string;
+    password: string;
+    addresses: Address[];
+    id: number;
+}
+
+interface Item {
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+    quantity: number;
+    user: number;
+    id: number;
+}
+
+export interface Order {
+    items: Item[];
+    totalAmount: number;
+    totalItems: number;
+    user: User;
+    paymentMethod: string;
+    selectedAddress: Address;
+    id: number;
+}
+
+export interface OrderState {
+    status: "idle" | "loading";
+    orders: Order[];
+    value: number;
+}
