13d92af891d3cdd1afc9e0e1be01e2696517dc0c
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2023-09-22 21:12:40 +0530

    Card Design Fix and Home Skeleton

--- client/src/app/Testing/loading.tsx
+++ client/src/app/Testing/loading.tsx
@@ -1,4 +1,3 @@
-"use client";
 import "./globals.css";
 import Banner from "@/app/components/Navbar/Banner/Home/Banner";
 import Navbar from "@/app/components/Navbar/Navbar";
@@ -7,18 +6,20 @@ import OurFacts from "@/app/components/siteStats/SiteStats";
 import Footer from "@/app/components/Footer/Footer";
 import { PcComponentProductList } from "@/app/components/products/pages/pc-components/ProductList";
 
-export default function Home() {
-  return (
-    <>
-      <div className="">
-        <main className="">
-          <Navbar />
-          <Banner />
-          <PcComponentProductList />
-          <OurFacts />
-        </main>
-      </div>
-      <Footer />
-    </>
-  );
+function Loading() {
+    return (
+        <>
+            loading .....
+            <div className="">
+                <main className="">
+                    <Navbar />
+                    <Banner />
+                    <PcComponentProductList />
+                    <OurFacts />
+                </main>
+            </div>
+        </>
+    );
 }
+
+export default Loading;
