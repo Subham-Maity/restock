b64b1e7f96a930963a83285e448d60c91ddfb84b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-09-26 02:00:34 +0530

    Logout Fix (router.push inside useEffect)

--- client/src/app/components/auth/components/Logout.tsx
+++ client/src/app/components/auth/components/Logout.tsx
@@ -1,14 +1,12 @@
 "use client"
 import { useEffect } from "react";
-
 import { useDispatch, useSelector } from "react-redux";
-import Link from "next/link";
 import { AppDispatch } from "@/lib/redux/store";
 import {
   selectLoggedInUser,
   signOutAsync,
 } from "@/app/components/auth/authSlice";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function Logout() {
   const dispatch: AppDispatch = useDispatch();
@@ -17,10 +15,13 @@ function Logout() {
 
   useEffect(() => {
     dispatch(signOutAsync());
-  });
 
-  return <>{!user && router.push("/")}</>;
+    if (!user) {
+      router.push("/");
+    }
+  }, [user, router, dispatch]);
+
+  return null;
 }
 
 export default Logout;
-
