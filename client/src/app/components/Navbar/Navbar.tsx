9676e4569925f85fe5432f8e844ed6f58a02afae
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-10-01 01:11:27 +0530

    Top Level Component Remove

--- client/src/app/components/Navbar/Navbar.tsx
+++ client/src/app/components/Navbar/Navbar.tsx
@@ -25,7 +25,7 @@ import { selectLoggedInUser } from "@/app/components/auth/authSlice";
 import { selectUserInfo } from "@/app/components/user/userSlice";
 
 import { selectAllProducts_ } from "@/app/components/products/pages/pc-components/productListSlice";
-import SearchProduct from "@/app/components/search/SearchProduct";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
 const navigation = [
   {
@@ -406,3 +406,60 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+export const SearchProduct = ({ items }: any) => {
+  const handleOnSearch = (string: any, results: any) => {
+    console.log(string, results);
+  };
+
+  const handleOnHover = (result: any) => {
+    console.log(result);
+  };
+
+  const handleOnSelect = (item: any) => {
+    console.log(item);
+  };
+
+  const handleOnFocus = () => {
+    console.log("Focused");
+  };
+
+  const formatResult = (items: any) => {
+    return (
+      <div className="flex focus:outline-none bg-cyan-200">
+        <div className="h-20 w-24 object-fill object-center">
+          <Image
+            className="w-full h-full object-fill object-center"
+            src={items.thumbnail}
+            alt={items.category}
+            height={100}
+            width={80}
+          />
+        </div>
+        <span style={{ display: "block", textAlign: "left" }}>
+          name: {items.title}
+        </span>
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{ width: 400 }}>
+          <ReactSearchAutocomplete
+            items={items}
+            fuseOptions={{ keys: ["title"] }}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+            className="focus:outline-none bg-cyan-500"
+          />
+        </div>
+      </header>
+    </div>
+  );
+};
