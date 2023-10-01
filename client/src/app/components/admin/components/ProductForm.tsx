afce0361f872b3fbe60cc466a964d16ada452cc2
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-10-02 04:19:26 +0530

    adminForm Small Css Fix

--- client/src/app/components/admin/components/ProductForm.tsx
+++ client/src/app/components/admin/components/ProductForm.tsx
@@ -10,12 +10,15 @@ function ProductForm() {
     <form>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 default-card">
         <div className="p-8">
-          <div className="border-b border-gray-900/10 ">
-            <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-200">
-              Add Product
-            </h2>
+          <div className="border-b dark:border-gray-400/25 border-gray-900/10 ">
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <h2 className="block leading-6 text-gray-700 dark:text-gray-400 text-2xl font-semibold ">
+               Product Update
+              </h2>
+            <div className="border-t mt-4 mb-2 border-gray-800 py-2 dark:border-gray-200  ">
+            </div>
+
+            <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-6">
                 <label
                   htmlFor="title"
@@ -312,7 +315,7 @@ function ProductForm() {
                 transition={{ duration: 0.5 }}
                 className="inline-block"
             >
-              <AiFillCloseSquare className="mt-1 mr-1" />
+              <AiFillCloseSquare className="mt-0.5 mr-1" />
             </motion.span>
             Cancel
           </motion.button>
@@ -334,14 +337,14 @@ function ProductForm() {
                 transition={{ duration: 0.5 }}
                 className="inline-block"
             >
-            <FaTrashAlt className="mt-1 mr-1" />
+            <FaTrashAlt className="mt-0.5 mr-1" />
             </motion.span>
            Delete
           </motion.button>
 
           <motion.button
               type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-indigo-600 px-3 py-2 mt-1 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               whileHover={{
                 scale: 1.05,
                 backgroundColor: "#4A90E2",
@@ -359,9 +362,9 @@ function ProductForm() {
                   },
                 }}
                 transition={{ duration: 0.5 }}
-                className="inline-block mr-1"
+                className="inline-block mr-1 "
             >
-              <FaSave className="mt-1 " />
+              <FaSave/>
             </motion.span>
             Save
           </motion.button>
