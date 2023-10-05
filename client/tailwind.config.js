043441e79604030bb20ca53d61104fd26c0ab6ed
Subhasish Negel <negelsubho@gmail.com>
2023-10-05 22:27:29 +0530

    Fixed some bugs on ProductListOnSearch.tsx

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xs': '460px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       height: {
         125: "30rem",
