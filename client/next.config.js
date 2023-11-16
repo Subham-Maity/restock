3e73aa3a336a84815e4bfc7fc56abd5030d91804
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-11-17 00:05:18 +0530

    Form connected Allow All Domain

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "tailwindui.com",
-      "images.unsplash.com",
-      "raw.githubusercontent.com",
-      "img.freepik.com",
-      "images.pexels.com",
-      "i.dummyjson.com",
-      "github.com",
-      "upload.wikimedia.org"
-    ],
+    remotePatterns:[
+      {
+        hostname: '**',
+      }
+    ]
+
   },
 };
 
