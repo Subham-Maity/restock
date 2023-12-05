cd97fc5e21e10a80c2784f470f0f1753345885df
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-12-05 20:13:20 +0530

    ⭐⭐⭐⭐⭐Server Re Structure ⭐⭐⭐⭐⭐

--- server/index.ts
+++ server/index.ts
@@ -3,35 +3,37 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import morgan, { FormatFn } from "morgan";
-import colors from "colors";
+import morgan from "morgan";
 import connectDB from "./config/dbConnection.js";
+
 
 /* CONFIG */
 dotenv.config();
 const server: Application = express();
 server.use(express.json());
 server.use(helmet());
-server.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-server.use(morgan("common" as FormatFn));
+server.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+server.use(morgan("common"));
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.urlencoded({extended: false}));
 server.use(cors());
 
 /* MONGOOSE SETUP */
 const PORT: string | number = process.env.PORT || 8000;
 server.get("/", (req: Request, res: Response) => {
-  res.send("Yes you are connected to the server! ✅");
+    res.send("Yes, you are connected to the server! ✅");
 });
 
-(async () =>
-  await connectDB()
-    .then(() => {
-      server.listen(PORT, () => {
-        console.log(`Server live on: ` + `http://localhost:${PORT}`);
-      });
-    })
-    .catch((err: Error) => {
-      console.log("Error: ", err);
-      throw new Error(err.message);
-    }))();
+
+(async () => {
+    await connectDB()
+        .then(() => {
+            server.listen(PORT, () => {
+                console.log(`Server live on: ` + `http://localhost:${PORT}`);
+            });
+        })
+        .catch((err: Error) => {
+            console.log("Error: ", err);
+            throw new Error(err.message);
+        });
+})();
