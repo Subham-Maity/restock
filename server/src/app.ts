2447c9630f8ea6c14944860def118ecbf12c6c0f
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-12-09 17:22:39 +0530

    Morgan Properly Setup For Security Purpose

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,8 +7,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { nanoid } from 'nanoid';
 import {corsUrl} from "./config.js";
 import ProductRouter from "./routes/products/product.router.js"
+import moment from "moment-timezone";
 
 
 /* CONFIG */
@@ -30,7 +32,29 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 
 // Middleware for logging HTTP requests - This will log HTTP requests to the console
-app.use(morgan("common"));
+app.use((req:any, res, next) => {
+    req.id = nanoid(); // Generates a unique ID for each request
+    req.requestOrigin = req.headers.origin || req.headers.referer; // Retrieves the request origin from headers
+    next(); // Passes control to the next middleware
+});
+morgan.token('id', function getId(req:any) {
+    return req.id;
+});
+
+morgan.token('date', function(req, res, tz) {
+    return moment().tz(<string>tz).format('YYYY-MM-DD HH:mm:ss.SSS');
+});
+
+morgan.token('origin', function getOrigin(req:any) {
+    return req.requestOrigin;
+});
+
+//Example: [1] c9uzb_V76qrKkV0EB6Zo0 - ::1 GET /api/v1/docs/favicon-32x32.png 304 3.050 ms - - 2023-12-09 17:02:13.971
+//id-unique id , origin - origin of the request , remote-addr - IP address of the client , method - HTTP method ,
+// url - URL of the request , status - HTTP status code , response-time - Time taken to respond in milliseconds ,
+// res[content-length] - Content length of the response , date - Date and time of the request
+app.use(morgan(':id :origin :remote-addr :method :url :status :response-time ms - :res[content-length] :date[Asia/Kolkata]'));
+
 
 // Middleware for parsing application/x-www-form-urlencoded - This will parse incoming requests with urlencoded payloads
 app.use(bodyParser.json());
