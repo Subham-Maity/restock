9d15e93cf0fc7e49a18b76fee9e1d6ed469458e7
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-02-10 16:33:48 +0530

    feat: stripe: fetching using react-query

--- client_user/src/components/payments/stripe/stripe-payment-intent.tsx
+++ client_user/src/components/payments/stripe/stripe-payment-intent.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 
@@ -8,27 +9,23 @@ import StripeCheckoutForm from "@/components/payments/stripe/stripe-form-checkou
 import { selectCurrentOrder } from "@/lib/features/order/order-slice";
 import { useAppSelector } from "@/store/redux/useSelector";
 import { stripePromise } from "@/utils/stripe/get-stripejs";
-
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
+import { useCreatePaymentIntent } from "@/lib/stripe/react-query-stripe";
 
 export default function StripeCheckout() {
   const [clientSecret, setClientSecret] = useState("");
   const currentOrder = useAppSelector(selectCurrentOrder);
+  const createPaymentIntent = useCreatePaymentIntent({
+    items: currentOrder.items,
+  });
 
   useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch(
-      "http://localhost:5050/api/v1/payments/stripe/create-payment-intent",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ items: currentOrder.items }),
-      },
-    )
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
+    if (createPaymentIntent.isSuccess) {
+      setClientSecret(createPaymentIntent.data);
+    }
+  }, [createPaymentIntent.isSuccess, createPaymentIntent.data]);
+
+  useEffect(() => {
+    createPaymentIntent.mutate();
   }, []);
 
   const appearance = {
