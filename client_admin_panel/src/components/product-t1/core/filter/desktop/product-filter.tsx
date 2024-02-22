ff34aa015d35d710dfb3def2bd24bd0a776a58f6
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-02-23 01:18:06 +0530

    feat: ⭐ Filter - Sheet & Search

--- client_admin_panel/src/components/product-t1/core/filter/desktop/product-filter.tsx
+++ client_admin_panel/src/components/product-t1/core/filter/desktop/product-filter.tsx
@@ -2,17 +2,30 @@ import { Disclosure } from "@headlessui/react";
 import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
 import { IFilter } from "@/types/utility/core/filter/filter.type";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/shadcn/sheet";
+import { ScrollArea } from "@/components/ui/shadcn/scroll-area";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/shadcn/command";
+
+const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
+
+const chooseSideLeft = SHEET_SIDES[3]; //0 1 2 3
+
 export const DesktopFilter = ({
   handleFilter,
   filters,
 }: {
   handleFilter: (e: any, section: any, option: any) => void;
   filters: IFilter[];
-  // TODO: The interface will initially display a maximum of 10 items.If there are more than 10 items, a ‘+231 more’ (or the actual number of additional items) indicator will be shown.Upon clicking this indicator,
-  // TODO: a modal window will open. This modal will contain cards representing each item, and it will have a fixed width and height that adapts to different screen sizes. At the top of the modal, there will be a search bar,
-  // TODO: accompanied by an alphabetical index (A, B, C, D, E, F, G, H… Z). Users can search for a specific brand or category by typing into the search bar. The search results will be displayed in the list below, categorized
-  // TODO: alphabetically. Additionally, users can quickly navigate to brands or categories starting with a specific letter by clicking on that letter in the alphabetical index at the top.
-  // TODO: For example, clicking on ‘B’ will display all brands and categories starting with ‘B’.The modal will also include a close button for users to easily exit the modal view.
 }) => {
   return (
     <>
@@ -41,25 +54,93 @@ export const DesktopFilter = ({
                 </h3>
                 <Disclosure.Panel className="pt-6">
                   <div className="space-y-4">
-                    {section.options.map((option: any, optionIdx: any) => (
-                      <div key={option.value} className="flex items-center">
-                        <input
-                          id={`filter-${section.id}-${optionIdx}`}
-                          name={`${section.id}[]`}
-                          defaultValue={option.value}
-                          type="checkbox"
-                          defaultChecked={option.checked}
-                          onChange={(e) => handleFilter(e, section, option)}
-                          className="h-4 w-4 rounded border-gray-300 text-[#2c515d]"
-                        />
-                        <label
-                          htmlFor={`filter-${section.id}-${optionIdx}`}
-                          className="ml-3 text-sm text-gray-600 dark:text-white dark:hover:text-[#5ccef8] "
-                        >
-                          {option.label}
-                        </label>
+                    {section.options
+                      .slice(0, 10)
+                      .map((option: any, optionIdx: any) => (
+                        <div key={option.value} className="flex items-center">
+                          <input
+                            id={`filter-${section.id}-${optionIdx}`}
+                            name={`${section.id}[]`}
+                            defaultValue={option.value}
+                            type="checkbox"
+                            defaultChecked={option.checked}
+                            onChange={(e) => handleFilter(e, section, option)}
+                            className="h-4 w-4 rounded border-gray-300 text-[#2c515d]"
+                          />
+                          <label
+                            htmlFor={`filter-${section.id}-${optionIdx}`}
+                            className="ml-3 text-sm text-gray-600 dark:text-white dark:hover:text-[#5ccef8] "
+                          >
+                            {option.label}
+                          </label>
+                        </div>
+                      ))}
+                    {section.options.length > 10 && (
+                      <div>
+                        <Sheet>
+                          <SheetTrigger className="text-md ml-2 text-sm text-gray-600 dark:text-white dark:hover:text-[#5ccef8] ">
+                            +{section.options.length - 10} more
+                          </SheetTrigger>
+                          <SheetContent
+                            side={chooseSideLeft}
+                            className="w-full"
+                          >
+                            <ScrollArea className="h-[600px] w-full rounded-md border p-4">
+                              <Command>
+                                <CommandInput
+                                  placeholder="Search for an option"
+                                  className="w-full dark:hover:bg-stone-800 shadow-2xl rounded-md h-9 mt-2 mb-2"
+                                />
+
+                                <CommandEmpty className="default-card-2">
+                                  No options found
+                                </CommandEmpty>
+                                <CommandGroup className="default-card-2">
+                                  {section.options
+                                    .slice(10)
+                                    .map((option: any, optionIdx: any) => (
+                                      <CommandItem
+                                        value={option.label}
+                                        key={option.value}
+                                        onSelect={() => {
+                                          handleFilter(null, section, option);
+                                        }}
+                                      >
+                                        <div
+                                          key={option.value}
+                                          className="flex items-center"
+                                        >
+                                          <input
+                                            id={`filter-${section.id}-${
+                                              optionIdx + 10
+                                            }`}
+                                            name={`${section.id}[]`}
+                                            defaultValue={option.value}
+                                            type="checkbox"
+                                            defaultChecked={option.checked}
+                                            onChange={(e) =>
+                                              handleFilter(e, section, option)
+                                            }
+                                            className="h-4 w-4 rounded border-gray-300 text-[#2c515d]"
+                                          />
+                                          <label
+                                            htmlFor={`filter-${section.id}-${
+                                              optionIdx + 10
+                                            }`}
+                                            className="ml-3 text-sm text-gray-600 dark:text-white dark:hover:text-[#5ccef8] "
+                                          >
+                                            {option.label}
+                                          </label>
+                                        </div>
+                                      </CommandItem>
+                                    ))}
+                                </CommandGroup>
+                              </Command>
+                            </ScrollArea>
+                          </SheetContent>
+                        </Sheet>
                       </div>
-                    ))}
+                    )}
                   </div>
                 </Disclosure.Panel>
               </>
